a7f0ec49cbfd9c78b052c1a893360d3f15558ea9
Amila <ireeshika@gmail.com>
2019-06-11 20:16:53 +0530

    Article ResourceFix

--- app/Http/Resources/Article/ArticleResource.php
+++ app/Http/Resources/Article/ArticleResource.php
@@ -18,8 +18,13 @@ class ArticleResource extends JsonResource
             'title' => $this->title,
             'content' => $this->details,
             'price' => $this->price,
-            'stock' => $this->availableCopies,
-            'dicount' => $this->discount
+            'stock' => $this->availableCopies == 0 ? 'No Stocks available' : $this->availableCopies,
+            'discount' => $this->discount == 0 ? 'No Discounts' : $this->discount,
+            'rating' => $this->feedbacks->count() > 0 ? round($this->feedbacks->sum('rating') / $this->feedbacks->count()) : 'No Ratings Yet',
+            'comments' => $this->feedbacks->count('commment') > 0 ? $this->feedbacks->count('commment') : 'Be the First one to Comment',
+            'href' => [
+                'feedbacks' => route('feedbacks.index', $this->id)
+            ]
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::Resource('/articles', 'TestApi\ArticleController');
 Route::apiResource('/articles', 'API\ArticleController');
 
 Route::group(['prefix' => 'articles'], function () {
